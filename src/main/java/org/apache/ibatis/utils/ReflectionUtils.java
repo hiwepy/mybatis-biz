ad2c2fd6222dad169467197814e8a9f10c7c340e
vindell <hnxyhcwdl1003@163.com>
2017-09-12 23:38:38 +0800

    xx

--- src/main/java/org/apache/ibatis/utils/ReflectionUtils.java
+++ src/main/java/org/apache/ibatis/utils/ReflectionUtils.java
@@ -26,7 +26,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
